b3f870cce3f833d9a529ce4bafa2624407c13fb8
Kerem Bozkurt <157289283+kerembozkurt2002@users.noreply.github.com>
2025-12-11 21:29:49 +0300

    make backend-frontend project

--- heatHMobile/services/semanticSearchService.ts
+++
@@ -1,7 +0,0 @@
-import { apiClient } from './apiClient';
-
-export const semanticSearchService = {
-  search: (query: string, topK: number = 10) => 
-    apiClient.post<any>('/search', { query, topK }).then((r: any) => r.data),
-};
-
